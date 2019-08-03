4f359775c2fedb1d135e6d5903b2dbc469ae48a6
shreyansh <shreyansh@localhost.localdomain>
2019-08-03 13:31:55 +0530

    KnapSack

---
+++ src/hello/DP6.java
@@ -0,0 +1,31 @@
+package hello;
+
+public class DP6 
+{
+	
+	public static int knapsack(int val[], int wt[],int W , int n)
+	{
+		
+		if(W == 0 || n == 0)
+			return 0;
+		if(wt[n-1]>W)
+			return knapsack(val,wt,W,n-1);
+		else
+			return Math.max((val[n-1]+knapsack(val,wt,W-wt[n-1],n-1)),(knapsack(val,wt,W,n-1)));
+		
+		
+		
+		
+	}
+	public static void main(String [] args)
+	{
+		int value[] = {60, 100, 120};
+		int wt[] = {10, 20, 30};
+		
+		int W = 50;
+		int n = value.length;
+		System.out.println(knapsack(value,wt,W,n));
+	}
+	
+
+}
